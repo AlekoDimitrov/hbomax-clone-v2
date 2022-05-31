233fff7d8c67bb7b63e6bcdeee2109af9cf710ab
Aleko Dimitrov <77157358+AlekoDimitrov@users.noreply.github.com>
2022-05-31 18:29:33 +0300

    feat: Make navigation arrows for movie sliders

--- src/Components/SecondaryScreen/SquareCard/SquareCard.tsx
+++ src/Components/SecondaryScreen/SquareCard/SquareCard.tsx
@@ -2,16 +2,21 @@ import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
 const SquareCard = (props) => {
-  const IMG_API = "https://image.tmdb.org/t/p/w500";
+  const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
   return (
     <Box
-      w={props.width ? props.width : "200px"}
+      w={props.width ? props.width : "210px"}
       mr={"20px"}
       pointerEvents={"none"}
       zIndex={1}
     >
-      <Image src={IMG_API + props.img} alt={props.title} cursor={"pointer"} />
+      <Image
+        src={IMG_URL + props.img}
+        alt={props.title}
+        cursor={"pointer"}
+        ref={props.reference}
+      />
       {props.titles && (
         <Text color={"whiteAlpha.800"} mt={"10px"}>
           {props.title}
