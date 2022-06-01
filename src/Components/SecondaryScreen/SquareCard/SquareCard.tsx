6be4a724f9cfec3761683636d791204582df5487
Aleko Dimitrov <77157358+AlekoDimitrov@users.noreply.github.com>
2022-06-01 11:57:34 +0300

    update: SquareCard title absolute position

--- src/Components/SecondaryScreen/SquareCard/SquareCard.tsx
+++ src/Components/SecondaryScreen/SquareCard/SquareCard.tsx
@@ -1,5 +1,6 @@
 import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import "./SquareCard.css";
 
 const SquareCard = (props) => {
   const IMG_URL = "https://image.tmdb.org/t/p/w500";
@@ -9,16 +10,30 @@ const SquareCard = (props) => {
       w={props.width ? props.width : "210px"}
       mr={"20px"}
       pointerEvents={"none"}
-      zIndex={1}
+      position={"relative"}
     >
+      {props.titles && (
+        <Box
+          position={"absolute"}
+          w={"100%"}
+          h={"100%"}
+          className={"backgroundShadow"}
+        />
+      )}
+
       <Image
         src={IMG_URL + props.img}
         alt={props.title}
-        cursor={"pointer"}
-        ref={props.reference}
-      />
+        className={"background"}
+      ></Image>
       {props.titles && (
-        <Text color={"whiteAlpha.800"} mt={"10px"}>
+        <Text
+          color={"whiteAlpha.900"}
+          w={"100%"}
+          position={"absolute"}
+          bottom={0}
+          left={2}
+        >
           {props.title}
         </Text>
       )}
